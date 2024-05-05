3d193f14e9d650cb94197b17001e771215526815
Lenivaya <xocada@gmail.com>
2024-05-05 21:23:42 +0300

    feat: Add general delivery state for status table
    Added a new column GeneralDeliveryState to the DeliveryStatus entity in the
    database for better tracking of parcel delivery states.

--- backend/ParcelTrackingService.DAL/Entities/DeliveryStatus.cs
+++ backend/ParcelTrackingService.DAL/Entities/DeliveryStatus.cs
@@ -2,8 +2,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ParcelTrackingService.DAL.Entities;
 
+public enum GeneralDeliveryState
+{
+    Sent,
+    InTransit,
+    Delivered,
+    Returned
+}
+
 [Table("delivery_statuses")]
 public class DeliveryStatus : BaseEntity
 {
     public string StatusName { get; init; } = string.Empty;
+    public GeneralDeliveryState GeneralDeliveryState { get; init; }
 }
---
+++ backend/ParcelTrackingService.DAL/Migrations/20240505182143_Add general delivery state for status table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelTrackingService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addgeneraldeliverystateforstatustable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GeneralDeliveryState",
+                table: "delivery_statuses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GeneralDeliveryState",
+                table: "delivery_statuses");
+        }
+    }
+}
